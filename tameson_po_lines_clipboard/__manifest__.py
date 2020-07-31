cfe92646a5db9a5480d6baa950cee9c226dd36b7
Administrator <nerds@therp.nl>
2020-07-31 11:10:27 +0000

    [ADD] #7626 add module tameson_po_lines_clipboard

---
+++ tameson_po_lines_clipboard/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Tameson PO Copy to Clipboard Button",
+    "version": "13.0.0.1.0",
+    "author": "Tameson",
+    "category": "General",
+    "summary": """Copy PO lines to clipboard""",
+    "description": """
+     Adds a button to copy purchase order lines to clipboard.
+    """,
+    "depends": [
+        "purchase",
+        "web",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/purchase_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
