b83ba0f3c40958dda7034e9a017655f108a6c509
arif4uw <arif4uw@Arifurs-MacBook-Pro.local>
2020-09-24 00:41:58 +0600

    [ADD] tameson_contact module Contact creation duplicate email constraint This will raise a validation error if contact with same email in system (not parent/child) exists.

---
+++ tameson_contact/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Tameson Contact Customizations',
+    'version': '13.0.0.0.0.0',
+    'description': """
+    * Contact creation duplicate email constraint
+    This will raise a validation error if contact with same email in system (not parent/child) exists.
+    """,
+    'author': "Tameson",
+    'depends': [
+        'base'
+    ],
+    'data': [
+    ],
+    'application': False,
+    'license': u'OPL-1',
+}
