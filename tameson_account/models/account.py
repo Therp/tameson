608fca4d75205843fdec42a85d5b7e3578a6ee0b
arif4uw <arif4uw@Arifurs-MacBook-Pro.local>
2020-10-04 21:29:33 +0600

    Code refactor

--- tameson_account/models/account.py
+++ tameson_account/models/account.py
@@ -13,17 +13,10 @@ class AccountMove(models.Model):
     def get_sale_order(self):
         if not self.invoice_origin:
             return
-
-        try:
-            return self.env['sale.order'].search([
-                ['name', '=', self.invoice_origin],
-            ])[0]
-        except IndexError:
-            pass
-
+        return self.env['sale.order'].search([('name', '=', self.invoice_origin)], limit=1)
 
     def get_picking_ids(self):
-        return self.mapped('line_ids').mapped('sale_line_ids').mapped('order_id').mapped('picking_ids')
+        return self.mapped('line_ids.sale_line_ids.order_id.picking_ids')
 
 class AccountPaymentTerm(models.Model):
     _inherit = 'account.payment.term'
