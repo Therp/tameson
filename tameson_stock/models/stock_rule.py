468f6d653871e2a53687931d1c5e663f2fb01227
arif4uw <arif4uw@gmail.com>
2021-01-14 18:05:45 +0600

    [FIX] fixed issue running scheduler

--- tameson_stock/models/stock_rule.py
+++ tameson_stock/models/stock_rule.py
@@ -13,15 +13,12 @@ from dateutil.relativedelta import relativedelta
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.osv import expression
 
+
 class ProcurementGroup(models.Model):
     _inherit = 'procurement.group'
 
-
     @api.model
-    def _get_moves_to_assign_domain(self):
+    def _get_moves_to_assign_domain(self, company_id):
         days = int(self.env["ir.config_parameter"].sudo().get_param("tameson_stock.auto_reservation_days", 7))
-        return expression.AND([
-            [('state', 'in', ['confirmed', 'partially_available'])],
-            [('product_uom_qty', '!=', 0.0)],
-            [('date_expected', '<=', (datetime.now() + relativedelta(days=days)).strftime(DEFAULT_SERVER_DATETIME_FORMAT))]
-        ])
+        moves_domain =  super(ProcurementGroup, self)._get_moves_to_assign_domain(company_id)
+        return expression.AND([moves_domain, [('date_expected', '<=', (datetime.now() + relativedelta(days=days)).strftime(DEFAULT_SERVER_DATETIME_FORMAT))]])
