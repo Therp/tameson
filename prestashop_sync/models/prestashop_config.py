4dfa729f3c9c390cd8618f3713c6327a61678243
Paul van Oorschot <pvoorschot@tameson.com>
2021-05-27 15:53:57 +0000

    add state  152  awaiting payment

--- prestashop_sync/models/prestashop_config.py
+++ prestashop_sync/models/prestashop_config.py
@@ -146,7 +146,8 @@ class PrestashopConfig(models.Model):
                     # CeleryTask.call_task('sale.order', 'update_from_prestashop', so_id=sale_order.id, order=order, celery=celery, 
                     #     celery_task_vals={'ref': 'Update existing order from prestashop: %s' % sale_order.name})
                     updated += 1
-                elif current_state in ('6', '13'):
+                    #skip states canceled,payment process started, awaiting payment (adyen)
+                elif current_state in ('6', '13', '152'):
                     skipped += 1
                 else:
                     customer = all_customer_data[order.get('id_customer', '0')]
