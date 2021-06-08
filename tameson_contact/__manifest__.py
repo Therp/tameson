3d80e37a7a5f4a4d8908597184680eada16fc6ae
Przemek Kaminski <pk@intrepidus.pl>
2021-06-08 10:35:40 +0200

    [tameson_contact] overwrite _merge check for sales_team.Administrator

--- tameson_contact/__manifest__.py
+++ tameson_contact/__manifest__.py
@@ -5,10 +5,14 @@
     'description': """
     * Contact creation duplicate email constraint
     This will raise a validation error if contact with same email in system (not parent/child) exists.
+
+    Also, this adds permissions for sales_team.Administrator users to merge
+    contacts.
     """,
     'author': "Tameson",
     'depends': [
-        'base_address_extended'
+        'base_address_extended',
+        'sales_team',
     ],
     'data': [
     ],
