5eedb1d72975390e3046ab9063dc432f283276ca
arif4uw <arif4uw@gmail.com>
2021-06-18 18:57:12 +0600

    [ADD] tameson_theme, no color bg change view file not added in manifest to keep default look

---
+++ tameson_theme/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': 'Tameson theme',
+    'summary': 'Tameson theme',
+    'version': '13.0.0.0.0',
+
+    'description': """
+Tameson theme
+==============================================
+This module will change odoo system accent color and background.
+
+
+    """,
+
+    'author': '',
+    'maintainer': '',
+    'contributors': [],
+
+    'website': '',
+
+    'category': 'Uncategorized',
+
+    'depends': [
+        'web',
+    ],
+    'external_dependencies': {
+        'python': [
+        ],
+    },
+    'data': [
+    ],
+    'installable': True
+}
