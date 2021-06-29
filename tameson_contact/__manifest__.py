348584b15a5f5607c141226935f7a12c0a9f3996
arif4uw <arif4uw@gmail.com>
2021-06-29 19:01:40 +0600

    [ADD] Readme for merge operation

--- tameson_contact/__manifest__.py
+++ tameson_contact/__manifest__.py
@@ -4,10 +4,12 @@
     'version': '13.0.0.0.0.0',
     'description': """
     * Contact creation duplicate email constraint
-    This will raise a validation error if contact with same email in system (not parent/child) exists.
-
-    Also, this adds permissions for sales_team.Administrator users to merge
-    contacts.
+      This will raise a validation error if contact with same email in system (not parent/child) exists.
+    * Also, this adds permissions for sales_team.Administrator users to merge contacts.
+    * This module will add the server action for admin/settings users to find and
+      merge all contacts having duplicate emails. Child contacts will be archived
+      if same zip code, will set as other address if different zip code, newest 
+      delivery and invoice contact will remain unchanged.
     """,
     'author': "Tameson",
     'depends': [
