443fe4ea4734de94156a06176b1111e6aa6f1c74
arif4uw <arif4uw@gmail.com>
2021-07-16 14:34:15 +0600

    [ADD] readme note

--- tameson_base/__manifest__.py
+++ tameson_base/__manifest__.py
@@ -4,6 +4,7 @@
     'version': '13.0.0.1.0.0',
     'description': """
     Tameson Base Customizations.
+    * Week of year set on ISO standard (4 day in January rule)
 
     Fields:
     - Model specific help text field.
--- tameson_base/static/src/js/session.js
+++ tameson_base/static/src/js/session.js
@@ -4,7 +4,8 @@ odoo.define('tameson_base.Session', function (require) {
 var session = require('web.Session');
 var core = require('web.core');
 var _t = core._t;
-
+// set `doy` for all locale to 4 days
+// ensures that week of the year according to ISO standard 4 days in january rule
 session.include({
     _configureLocale: function () {
         moment.updateLocale(moment.locale(), {
