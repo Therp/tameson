141f3e8bce119e16e365101da73956d00632b754
arif4uw <arif4uw@gmail.com>
2021-08-11 04:20:10 +0600

    [ADD] modification time based updates

--- tameson_pimcore/models/product_response.py
+++ tameson_pimcore/models/product_response.py
@@ -5,6 +5,7 @@
 #    __manifest__.py file at the root folder of this module.                  #
 ###############################################################################
 
+import enum
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 import requests, codecs
@@ -50,64 +51,34 @@ class PimcoreProductResponseLine(models.Model):
     config_id = fields.Many2one(comodel_name='pimcore.config', ondelete='cascade',)
     
     def create_products(self):
-        Category = self.env['product.category']
-        Products = self.env['product.template'].search([])
-        Skus = Products.mapped('default_code')
-        new_lines = self.env['pimcore.product.response.line'].search([('response_id','=',self.id),('sku','not in',Skus),('state','=','draft')])[:100]
+        self.env.cr.execute("""
+        select rl.id, pt.id, rl.modification_date, coalesce(pt.modification_date, 0) from pimcore_product_response_line rl
+        left join product_template pt on rl.sku = pt.default_code
+        where rl.state = 'draft'
+        """)
+        data = self.env.cr.fetchall()
+        skipped = [row[0] for row in data if row[2] <= row[3]]
+        Line = self.env['pimcore.product.response.line']
+        self.env['pimcore.product.response.line'].browse(skipped).write({'state': 'skipped'})
         Eur = self.env['product.pricelist'].search([('currency_id','=','EUR')], limit=1)
         Gbp = self.env['product.pricelist'].search([('currency_id','=','GBP')], limit=1)
         Usd = self.env['product.pricelist'].search([('currency_id','=','USD')], limit=1)
-        _logger.warning('start')
-        for line in new_lines:
-            image_response = requests.get('%s/%s' % (self.config_id.api_host, line.image))
-            if image_response.status_code == 200:
-                image_data = codecs.encode(image_response.content, 'base64')
-            else:
-                image_data = False
-            categ_path = line.full_path.split('/')[3:]
-
-            child_categ = self.env['product.category']
-            final_categ = self.env['product.category']
-            for categ in categ_path[::-1]:
-                break_loop = False
-                this_categ = Category.search([('name','=',categ)], limit=1)
-                if not this_categ:
-                    this_categ = Category.create({'name': categ})
-                else:
-                    break_loop = True
-                child_categ.parent_id = this_categ
-                child_categ = this_categ
-                if not final_categ:
-                    final_categ = this_categ
-                if break_loop:
-                    break
-
-            product = self.env['product.template'].create({
-                'name': line.name,
-                'default_code': line.sku,
-                'weight': line.weight,
-                't_height': line.height,
-                't_length': line.depth,
-                't_width': line.width,
-                'type': 'product',
-                'list_price': line.wholesaleprice,
-                'image_1920': image_data,
-                'categ_id': final_categ.id
-            })
-            add_translation(self.env, product, 'nl_NL', line.name, line.name_nl)
-            add_pricelist_item(Eur, product, line.eur)
-            add_pricelist_item(Gbp, product, line.gbp)
-            add_pricelist_item(Usd, product, line.usd)
-        new_lines.write({'state': 'created'})
-        self.message_post(body = 'Products created %d' % len(new_lines))
-        _logger.warning('end')
-
+        for count, row in enumerate(data):
+            _logger.info("%d / %d %s" % (count, len(data), row))
+            try:
+                if not row[1]:
+                    Line.browse(row[0]).create_product(Eur, Gbp, Usd)
+                elif row[2] > row[3]:
+                    Line.browse(row[0]).update_product(row[1])
+            except Exception as e:
+                self.message_post(body = str(e))
+                _logger.warn(str(e))
+                Line.browse(row[0]).write({'state': 'error'})
 
 class PimcoreProductResponseLine(models.Model):
     _name = 'pimcore.product.response.line'
     _description = 'PimcoreProductResponseLine'
 
-    
     state = fields.Selection(string='Status', default='draft',
         selection=[('draft', 'Draft'), ('created', 'Created'), ('updated', 'Updated'), ('skipped', 'Skipped'), ('error', 'Error')])
     response_id = fields.Many2one(comodel_name='pimcore.product.response', ondelete='restrict',)
@@ -130,4 +101,56 @@ class PimcoreProductResponseLine(models.Model):
     usd = fields.Float()
     published = fields.Boolean()
     
+    def create_product(self, Eur, Gbp, Usd):
+        Category = self.env['product.category']
+        image_response = requests.get('%s/%s' % (self.response_id.config_id.api_host, self.image))
+        if image_response.status_code == 200:
+            image_data = codecs.encode(image_response.content, 'base64')
+        else:
+            image_data = False
+        categ_path = self.full_path.split('/')[3:]
+        child_categ = self.env['product.category']
+        final_categ = self.env['product.category']
+        for categ in categ_path[::-1]:
+            break_loop = False
+            this_categ = Category.search([('name','=',categ)], limit=1)
+            if not this_categ:
+                this_categ = Category.create({'name': categ})
+            else:
+                break_loop = True
+            child_categ.parent_id = this_categ
+            child_categ = this_categ
+            if not final_categ:
+                final_categ = this_categ
+            if break_loop:
+                break
+        vals = self.get_product_vals()
+        vals.update({
+            'image_1920': image_data,
+            'categ_id': final_categ.id,
+        })
+        product = self.env['product.template'].create(vals)
+        add_translation(self.env, product, 'nl_NL', self.name, self.name_nl)
+        add_pricelist_item(Eur, product, self.eur)
+        add_pricelist_item(Gbp, product, self.gbp)
+        add_pricelist_item(Usd, product, self.usd)
+        self.write({'state': 'created'})
+        self.env.cr.commit()
+    
+    def update_product(self, product_id):
+        self.env['product.template'].browse(product_id).write(self.get_product_vals())
+        self.write({'state': 'updated'})
+        self.env.cr.commit()
 
+    def get_product_vals(self):
+        return {
+            'name': self.name,
+            'default_code': self.sku,
+            'weight': self.weight,
+            't_height': self.height,
+            't_length': self.depth,
+            't_width': self.width,
+            'type': 'product',
+            'list_price': self.wholesaleprice,
+            'modification_date': self.modification_date
+        }
