39ac110efe11709e658ce18cf7a5572d03d2902e
Paul van Oorschot <pvoorschot@tameson.com>
2021-12-09 14:35:26 +0000

    [UPD] contact matched changed data update

---
+++ tameson_shopify/models/stock_level.py
@@ -0,0 +1,31 @@
+
+# -*- coding: utf-8 -*-
+###############################################################################
+#    License, author and contributors information in:                         #
+#    __manifest__.py file at the root folder of this module.                  #
+###############################################################################
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+
+
+class ShopifyStockLevel(models.Model):
+    _name = 'shopify.stock.level'
+    _description = 'Shopify Stock Level'
+
+    _rec_name = 'name'
+    _order = 'name ASC'
+
+    name = fields.Char(string='SKU', required=True,)
+    inventory_item_id = fields.Char(required=True)
+    variant_id = fields.Char(required=True)
+    shopify_tmpl_id = fields.Char(required=True)
+    available = fields.Float()
+    
+    @api.model_create_multi
+    def create(self, vals_list):
+        self.search([]).unlink()
+        result = super(ShopifyStockLevel, self).create(vals_list)    
+        return result
+    
