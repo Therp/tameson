b9baf4cc0cd0e0be36fd0c745302f2c39c903441
arahman <arahman@tameson.com>
2021-12-27 13:14:30 +0600

    [ADD] deduct tax from line price

--- tameson_shopify/models/sale.py
+++ tameson_shopify/models/sale.py
@@ -34,7 +34,7 @@ class SaleOrder(models.Model):
     def import_shopify_orders(self, order_data_queue_line, log_book_id):
         order_id = super(SaleOrder, self).import_shopify_orders(order_data_queue_line, log_book_id)
         total_price = float(json.loads(order_data_queue_line.order_data).get('order',{}).get('total_price',0))
-        if float_compare(total_price, order_id.amount_total, precision_digits=2) != 0:
+        if order_id and float_compare(total_price, order_id.amount_total, precision_digits=2) != 0:
             raise UserError("Total amount missmatch %.2f %.2f" % (total_price, order_id.amount_total))
         return order_id
 
@@ -47,7 +47,7 @@ class SaleOrder(models.Model):
                                        product_name, order_id,
                                        price, order_response, is_shipping,
                                        previous_line, is_discount)
-        tax =  sum([float(l['price']) for l in line['tax_lines']])
-        line_id.price_unit -= tax
+        tax =  sum([float(l['rate']) for l in line['tax_lines']])
+        line_id.price_unit = line_id.price_unit * (1 - tax)
         line_id.with_context(round=False)._compute_amount()
         return line_id
