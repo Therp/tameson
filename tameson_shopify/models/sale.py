732a7d7505de7df004756d7dd4a630eaf140298d
arahman <arahman@tameson.com>
2021-12-27 16:04:13 +0600

    [UPD] price unit = price / (1 + tax)

--- tameson_shopify/models/sale.py
+++ tameson_shopify/models/sale.py
@@ -35,7 +35,7 @@ class SaleOrder(models.Model):
         order_id = super(SaleOrder, self).import_shopify_orders(order_data_queue_line, log_book_id)
         total_price = float(json.loads(order_data_queue_line.order_data).get('order',{}).get('total_price',0))
         if order_id and float_compare(total_price, order_id.amount_total, precision_digits=2) != 0:
-            raise UserError("Total amount missmatch %.2f %.2f" % (total_price, order_id.amount_total))
+            raise UserError("Total amount missmatch shopify: %.2f odoo: %.2f" % (total_price, order_id.amount_total))
         return order_id
 
     def shopify_create_sale_order_line(self, line, product, quantity,
@@ -48,6 +48,6 @@ class SaleOrder(models.Model):
                                        price, order_response, is_shipping,
                                        previous_line, is_discount)
         tax =  sum([float(l['rate']) for l in line['tax_lines']])
-        line_id.price_unit = line_id.price_unit * (1 - tax)
+        line_id.price_unit = line_id.price_unit / (1 + tax)
         line_id.with_context(round=False)._compute_amount()
         return line_id
