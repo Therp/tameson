3787beadf09b541d0b7b1f310b49d99a0efc30af
arahman <arahman@tameson.com>
2022-01-18 16:31:17 +0600

    [ADD] check presta cred from controller

--- tameson_presta_auth/models/users.py
+++ tameson_presta_auth/models/users.py
@@ -14,24 +14,24 @@ salt = '8kSCkZeFekwd0tX4A2FMAIqq6mnBKWpRFjJH6iT0K5arcjUfHBZWTBtK'
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    @classmethod
-    def _login(cls, db, login, password):
-        try:
-            return super(ResUsers, cls)._login(db, login, password)
-        except AccessDenied:
-            with cls.pool.cursor() as cr:
-                env = api.Environment(cr, SUPERUSER_ID, {})
-                UserModel = env[cls._name]
-                user = UserModel.search(UserModel._get_login_domain(login), order=UserModel._get_login_order(), limit=1)
-                if user:
-                    raise AccessDenied()
-                PrestaUserModel = env['presta.users']
-                with UserModel._assert_can_auth():
-                    presta_user = PrestaUserModel.search([('login','=',login)], limit=1)
-                    if not presta_user:
-                        raise AccessDenied()
-                    user = presta_user._check_hash(password)
-                    raise AccessDenied("User migrated from old system, please login again.")
+    # @classmethod
+    # def _login(cls, db, login, password):
+    #     try:
+    #         return super(ResUsers, cls)._login(db, login, password)
+    #     except AccessDenied:
+    #         with cls.pool.cursor() as cr:
+    #             env = api.Environment(cr, SUPERUSER_ID, {})
+    #             UserModel = env[cls._name]
+    #             user = UserModel.search(UserModel._get_login_domain(login), order=UserModel._get_login_order(), limit=1)
+    #             if user:
+    #                 raise AccessDenied()
+    #             PrestaUserModel = env['presta.users']
+    #             with UserModel._assert_can_auth():
+    #                 presta_user = PrestaUserModel.search([('login','=',login)], limit=1)
+    #                 if not presta_user:
+    #                     raise AccessDenied()
+    #                 user = presta_user._check_hash(password)
+    #                 raise AccessDenied("User migrated from old system, please login again.")
 
 
 class PrestaUsers(models.Model):
@@ -43,6 +43,7 @@ class PrestaUsers(models.Model):
 
     login = fields.Char(required=True)
     hashpw = fields.Char(required=True)
+    active = fields.Boolean(default=True)
 
     _sql_constraints = [
         (
@@ -66,7 +67,7 @@ class PrestaUsers(models.Model):
     def _check_hash(self, password=None):
         self.ensure_one()
         if not password:
-            raise AccessDenied()
+            return
         match = False
         hashpw = self.hashpw.encode('utf-8')
         login = self.login
@@ -76,27 +77,24 @@ class PrestaUsers(models.Model):
             md5_hash = hashlib.md5((salt + password).encode('utf-8')).hexdigest()
             if bcrypt.checkpw(md5_hash.encode('utf-8'), hashpw):
                 match = True
-        if match:
-            ResPartner = self.env['res.partner']
-            partner = ResPartner.search([('email','=',self.login)], limit=1, order='parent_id DESC')
-            if not partner:
-                partner = ResPartner.create({'name': login, 'email': login})
-            wizard = self.env['portal.wizard'].with_context(active_ids=partner.ids).create({})
-            wizard.user_ids.write({'in_portal': True})
-            wizard.action_apply()
-            user = partner.user_ids[:1]
-            cp_wiz = self.env['change.password.wizard'].create({
-                'user_ids': [(0, 0, {
-                    'user_id': user.id,
-                    'user_login': login,
-                    'new_passwd': password
-                })]
-            })
-            cp_wiz.change_password_button()
-            self.env.cr.commit()
-        else:
-            raise AccessDenied()
-        return user
-    
-
+        if not match:
+            return
+        ResPartner = self.env['res.partner']
+        partner = ResPartner.search([('email','=',self.login)], limit=1, order='parent_id DESC')
+        if not partner:
+            partner = ResPartner.create({'name': login, 'email': login})
+        wizard = self.env['portal.wizard'].with_context(active_ids=partner.ids).create({})
+        wizard.user_ids.write({'in_portal': True})
+        wizard.action_apply()
+        user = partner.user_ids[:1]
+        cp_wiz = self.env['change.password.wizard'].create({
+            'user_ids': [(0, 0, {
+                'user_id': user.id,
+                'user_login': login,
+                'new_passwd': password
+            })]
+        })
+        cp_wiz.change_password_button()
+        self.write({'active': False})
+        self.env.cr.commit()
     
