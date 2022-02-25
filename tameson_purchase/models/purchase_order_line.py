00104f7af03683b279e1cc6dbedd3b5b560700bf
arahman <arahman@tameson.com>
2022-02-25 14:14:52 +0600

    [ADD] rfq minimal_qty_available max_reorder added

--- tameson_purchase/models/purchase_order_line.py
+++ tameson_purchase/models/purchase_order_line.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import models, fields, _
 
 
 class PurchaseOrderLine(models.Model):
@@ -38,3 +38,17 @@ class PurchaseOrderLine(models.Model):
             company_id,
             values,
         )
+
+    minimal_qty_available = fields.Float(
+        related="product_id.minimal_qty_available",
+        string=_("Product min qty available"),
+        readonly=True,
+    )
+    
+    max_reorder = fields.Float(
+        compute='_get_max_reorder'
+    )
+    
+    def _get_max_reorder(self):
+        for line in self:
+            line.max_reorder = line.product_id.orderpoint_ids[:1].product_max_qty
