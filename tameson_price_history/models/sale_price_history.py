2b6e2bdf62800aae53aad0b5fa55759b068d5287
arahman <arahman@tameson.com>
2022-03-28 11:25:52 +0600

    [UPD] bar graph view

--- tameson_price_history/models/sale_price_history.py
+++ tameson_price_history/models/sale_price_history.py
@@ -24,41 +24,6 @@ class SalePriceHistory(models.Model):
         default=fields.Date.context_today,
     )
 
-
-class ReportSPH(models.Model):
-    _name = "report.sale.price.history"
-    _auto = False
-
-    product_tmpl_id = fields.Many2one(
-        string="Product", comodel_name="product.template", ondelete="cascade"
-    )
-    sale_price = fields.Float()
-    date = fields.Date(
-        string="Date",
-        default=fields.Date.context_today,
-    )
-
-    def init(self):
-        tools.drop_view_if_exists(self._cr, "report_sale_price_history")
-        query = """
-CREATE or REPLACE VIEW report_sale_price_history AS (
-with main as (
-	select dt::date date, pt.id product_tmpl_id
-	from generate_series((now() at time zone 'utc')::date - interval '6month',
-        (now() at time zone 'utc')::date, '7 day'::interval) dt, 
-	product_template pt 
-	where pt.id in (select distinct product_tmpl_id from sale_price_history)
-	order by dt DESC, pt.id asc
-)
-select row_number() over (order by main.date, main.product_tmpl_id) id, main.*, 
-		(select sph.sale_price from sale_price_history sph
-		 where sph.date <= main.date and main.product_tmpl_id = sph.product_tmpl_id
-		 limit 1
-			   )
-from main)"""
-        self.env.cr.execute(query)
-
-
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
