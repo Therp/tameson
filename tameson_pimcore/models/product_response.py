ea631b312ad76436f0de5a421d24e748060ba31e
arahman <arahman@tameson.com>
2022-05-06 12:44:35 +0600

    [ADD] don't archive when active po, so, move

--- tameson_pimcore/models/product_response.py
+++ tameson_pimcore/models/product_response.py
@@ -191,6 +191,13 @@ SELECT rl.id, pt.id, rl.modification_date, coalesce(pt.modification_date, 0) FRO
         unpublished_products = self.env["product.template"].search(
             [("published", "=", False)]
         )
+        unpublished_product_variants = unpublished_products.mapped('product_variant_ids')
+        active_products = self.emv['stock.move'].search([('product_id','in',unpublished_product_variants.ids,('state','not in',('done','cancel')))]).mapped('product_id')
+        active_products += self.emv['purchase.order.line'].search([('product_id','in',unpublished_product_variants.ids),('state','=','draft')]).mapped('product_id')
+        active_products += self.emv['sale.order.line'].search([('product_id','in',unpublished_product_variants.ids),('state','=','draft')]).mapped('product_id')
+        active_pts = active_products.mapped('product_tmpl_id')
+        _logger.info("%s not archived from pimcore response due to active pos/so/move" % active_pts.mapped('default_code'))
+        unpublished_products = unpublished_products - active_pts
         self.env["stock.warehouse.orderpoint"].search(
             [
                 (
@@ -201,6 +208,7 @@ SELECT rl.id, pt.id, rl.modification_date, coalesce(pt.modification_date, 0) FRO
             ]
         ).action_archive()
         unpublished_products.action_archive()
+        _logger.info("%s archived from pimcore response" % unpublished_products.mapped('default_code'))
         published_products = self.env["product.template"].search(
             [("published", "=", True), ("active", "=", False)]
         )
