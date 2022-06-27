51164263d40a0c2e92b5a367581db57ab8d7daae
arahman <arahman@tameson.com>
2022-06-27 14:10:58 +0600

    add component for invoice

---
+++ account_invoice_import_edi/components/process.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Camptocamp SA
+# @author: Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from odoo import _, api
+from odoo.exceptions import UserError
+
+import logging
+_logger = logging.getLogger(__name__)
+
+from odoo.addons.component.core import Component
+
+class EDIExchangeInvoiceInput(Component):
+    """Process sale orders."""
+
+    _name = "edi.input.invoice.landefeld.process"
+    _inherit = "edi.component.input.mixin"
+    _usage = "input.process.invoice.landefeld"
+
+    def process(self):
+        _logger.info("Process called")
