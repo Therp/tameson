035469a20b97ad637ff676ebec4bb329676ab5a3
arahman <arahman@tameson.com>
2022-08-02 18:32:04 +0600

    generate cart from request params

--- tameson_shopify/controllers/main.py
+++ tameson_shopify/controllers/main.py
@@ -24,7 +24,7 @@ class Shopify(Controller):
     @route(['/shopify/cart_migrate'], type='http', auth="user", website=True, methods=["POST"], csrf=False)
     def shopify_cart_migration(self, data):
         data = json.loads(data)
-        order = request.website.sale_get_order()
+        order = request.website.sale_get_order(force_create=True)
         order.sudo().order_line.unlink()
         for item in data.get('items', []):
             sku = item['sku']
