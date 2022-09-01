0eeac06b4d15d483134109c0be69e5f9b39587c2
arahman <arahman@tameson.com>
2022-09-01 21:39:01 +0600

    webhook implementation

--- tameson_shopify/models/shopify.py
+++ tameson_shopify/models/shopify.py
@@ -146,8 +146,8 @@ class ShopifyInstanceEpt(models.Model):
     def delete_bulk_export_wh(self):
         session = shopify.Session(self.shopify_host, "2021-04", self.shopify_password)
         shopify.ShopifyResource.activate_session(session)
-        webhook = shopify.Webhook()
-        webhook.destroy(self.export_done_webhook)
+        webhook = shopify.Webhook().find(self.export_done_webhook)
+        webhook.destroy()
         self.export_done_webhook = False
 
 class ResCountry(models.Model):
