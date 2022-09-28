30c585f0a13505eca6e382c6bbc4a3ca3a2001e3
arahman <arahman@tameson.com>
2022-09-28 10:10:27 +0600

    vat editable

---
+++ tameson_website/models/res_partner.py
@@ -0,0 +1,18 @@
+
+# -*- coding: utf-8 -*-
+###############################################################################
+#    License, author and contributors information in:                         #
+#    __manifest__.py file at the root folder of this module.                  #
+###############################################################################
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    
+    def can_edit_vat(self):
+        if not (self.vat or self.parent_id):
+            return True
