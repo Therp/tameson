52acc0f77e8d7c4802819c8da4afe71b49839af1
arahman <arahman@tameson.com>
2023-01-05 11:36:54 +0600

    add average_payment_days

--- tameson_account/models/res_partner.py
+++ tameson_account/models/res_partner.py
@@ -13,4 +13,17 @@ class ResPartner(models.Model):
 
     
     is_product_supplier = fields.Boolean()
+    average_payment_days = fields.Float(compute='get_average_payment_days')
     
+    def get_average_payment_days(self):
+        for partner in self:
+            all_child = self.with_context(active_test=False).search([('id', 'child_of', partner.ids)])
+            payments = self.env['account.payment'].search([('partner_id', 'in', all_child.ids),('payment_type','=','inbound')])
+            days = []
+            for payment in payments:
+                for invoice in payment.invoice_ids:
+                    days.append((payment.payment_date - invoice.date).days)
+            if days:
+                partner.average_payment_days = sum(days) / len(days)
+            else:
+                partner.average_payment_days = 0
