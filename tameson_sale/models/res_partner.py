d5913f57baf890e0641a17c8ede2b6ba7a9f8814
arahman <arahman@tameson.com>
2023-02-27 08:29:33 +0600

    partner lang on create function

--- tameson_sale/models/res_partner.py
+++ tameson_sale/models/res_partner.py
@@ -13,18 +13,12 @@ from odoo.exceptions import UserError, ValidationError
 class ResPartnet(models.Model):
     _inherit = 'res.partner'
 
-    #On partner creation view, automatically change partner language to default language for partners country
-    @api.onchange('country_id')
-    def onchange_country_lang(self):
-        if self.country_id.select_lang:
-            self.lang = self.country_id.select_lang
-
-    
     @api.model_create_multi
     def create(self, vals_list):
         partners = super().create(vals_list)
         for partner in partners:
-            if partner.parent_id:
-                continue
-            partner.property_product_pricelist = partner.country_id.select_pricelist_id
+            if not partner.lang:
+                partner.lang = partner.country_id.select_lang or 'en_US'
+            if not partner.parent_id:
+                partner.property_product_pricelist = partner.country_id.select_pricelist_id
         return partners
