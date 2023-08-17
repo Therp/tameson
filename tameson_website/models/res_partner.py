4b37b8b3dfc46b2a356a420a34be554b2606d36f
arahman <arahman@tameson.com>
2023-08-17 14:42:39 +0600

    translation value for address type

--- tameson_website/models/res_partner.py
+++ tameson_website/models/res_partner.py
@@ -3,7 +3,15 @@
 #    __manifest__.py file at the root folder of this module.                  #
 ###############################################################################
 
-from odoo import models
+from odoo import _, fields, models
+
+
+def get_selection_label(self, object, field_name, field_value):
+    return _(
+        dict(
+            self.env[object].fields_get(allfields=[field_name])[field_name]["selection"]
+        )[field_value]
+    )
 
 
 class ResPartner(models.Model):
@@ -14,3 +22,8 @@ class ResPartner(models.Model):
             return True
         else:
             return super(ResPartner, self).can_edit_vat()
+
+    type_name = fields.Char(compute="_get_type_name")
+
+    def _get_type_name(self):
+        self.type_name = get_selection_label(self, "res.partner", "type", self.type)
