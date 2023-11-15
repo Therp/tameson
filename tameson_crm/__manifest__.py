6b4a5f8d8a69d0656f92d0d95a5f0b74fa4af08c
Danny de Jong <ddejong@therp.nl>
2023-11-15 12:45:48 +0100

    [ADD] #10095 Added tameson_crm.

---
+++ tameson_crm/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Tameson CRM Customizations",
+    "version": "16.0.1.0.0",
+    "description": """
+    Tameson CRM Customizations.
+    """,
+    "author": "Tameson",
+    "depends": [
+        "crm",
+        "tameson_base",
+    ],
+    "data": [
+        "views/crm_views.xml",
+    ],
+    "application": False,
+    "license": "OPL-1",
+}
