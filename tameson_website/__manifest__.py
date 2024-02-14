ca41f05ab337c6e5f7de5f2c697b1d782ad37262
arahman <arahman@tameson.com>
2024-02-14 22:22:36 +0600

    test mollie issue hack

--- tameson_website/__manifest__.py
+++ tameson_website/__manifest__.py
@@ -15,6 +15,7 @@
         "payment_custom",
         "sale_stock",
         "sale",
+        "payment_mollie_official",
     ],
     "data": [
         "security/ir.model.access.csv",
@@ -31,6 +32,8 @@
         "web.assets_frontend": [
             "tameson_website/static/src/js/payment.js",
             "tameson_website/static/src/scss/tameson.scss",
+            "payment_mollie_official/static/src/js/payment_form.js",
+            "payment_mollie_official/static/src/js/qr_dialog.js",
         ],
     },
 }
